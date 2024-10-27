755ef2f5e02d6cf5c8e75b27c51fd02b4eb6ca7f
Yura <urijkisil2007@gmail.com>
2024-10-27 20:22:21 +0200

    first master

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,44 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_8_1_string/lab_8_1_string.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestCount)
+		{
+			string str1 = "while we wait, while working.";
+			string str2 = "no while here";
+			string str3 = "whilewhile";
+
+			Assert::AreEqual(2, Count(str1, "while"));
+			Assert::AreEqual(1, Count(str2, "while"));
+			Assert::AreEqual(2, Count(str3, "while"));
+		}
+		TEST_METHOD(TestChange)
+		{
+			string str1 = "while we wait, while working.";
+			string expected1 = "*** we wait, *** working.";
+			string str2 = "no while here";
+			string expected2 = "no *** here";
+			string str3 = "whilewhile";
+			string expected3 = "******";
+
+			// Виконуємо заміну
+			Change(str1);
+			Change(str2);
+			Change(str3);
+
+			// Перевіряємо результати, використовуючи IsTrue з оператором порівняння ==
+			Assert::IsTrue(str1 == expected1);
+			Assert::IsTrue(str2 == expected2);
+			Assert::IsTrue(str3 == expected3);
+		}
+
+	};
+}
---
+++ lab_8_1_string/lab_8_1_string.cpp
@@ -0,0 +1,42 @@
+﻿// lab_8_1_string.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+int Count(const string s, const string cs)
+{
+	size_t pos = 0;
+	int k = 0;
+	while ((pos = s.find(cs, pos)) != -1)
+	{
+		k++;
+		pos++;
+	}
+	return k;
+}
+
+
+string Change(string& s)
+{
+	size_t pos = 0;
+	while ((pos = s.find('w', pos)) != -1)
+		if (s[pos + 1] == 'h' && s[pos + 2] == 'i' && s[pos + 3] == 'l' && s[pos + 4] == 'e')
+			s.replace(pos, 5, "***");
+		else
+			pos++;
+	return s;
+}
+int main()
+{
+	string str;
+	cout << "Enter string:" << endl;
+	getline(cin, str);
+	string cs = "while";
+	cout << "String contained " << Count(str, cs) << " groups of 'while'" << endl;
+	string dest = Change(str);
+	cout << "Modified string (param) : " << str << endl;
+	cout << "Modified string (result): " << dest << endl;
+	return 0;
+}
